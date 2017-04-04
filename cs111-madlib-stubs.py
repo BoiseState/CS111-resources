4174762ae804ef7eb43605957c9dc1eb4440d7f7
jjones-bsu <jjones.bsu@gmail.com>
2017-04-04 11:26:36 -0600

    Add files via upload

---
+++ cs111-madlib-stubs.py
@@ -0,0 +1,45 @@
+#
+# Author: 
+# Date: 
+# Description:
+#
+# This MadLib will require the following categories
+#  adj  - http://examples.yourdictionary.com/examples-of-adjectives.html
+#  noun  - http://examples.yourdictionary.com/noun-examples.html
+#  adverb  - http://examples.yourdictionary.com/examples-of-adverbs.html
+#  exclamation  - http://examples.yourdictionary.com/examples-of-interjections.html
+
+import random
+
+#
+# Define CONSTANT List variables for each category
+#
+ADJ_LIST = []
+NOUN_LIST = []
+ADVERB_LIST = []
+EXCLAMATION_LIST = []
+
+                
+               
+#
+# This function is an implementation of the Newspaper Article Mad Lib.
+#   It will dynamically generate a new Mad Lib each time it is called by
+#   randomly selecting values from the above lists
+# Return
+# This function will return a String containing the new Mad Lib
+def generateMadLib():
+
+
+
+
+#
+# Generate the MadLib
+#
+madlib = generateMadLib()
+
+#
+# Print the MadLib
+#
+print(madlib)
+
+
