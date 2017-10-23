07849404aae812a17167e03ecb80795350cafd7a
Luke Hindman <luke@platypuscreations.net>
2017-10-22 19:56:04 -0600

    Added new folder with stubs for minitasks

---
+++ minitasks/cs111-madlib-stubs.py
@@ -0,0 +1,45 @@
+#
+# Author: 
+# Date: 
+# Description:
+#
+# This Madlib will require the following categories
+#  adj  - http://examples.yourdictionary.com/examples-of-adjectives.html
+#  noun  - http://examples.yourdictionary.com/noun-examples.html
+#  adverb  - http://examples.yourdictionary.com/examples-of-adverbs.html
+#  exclamation  - http://examples.yourdictionary.com/examples-of-interjections.html
+
+import random
+
+#
+# Define CONSTANT List variables for each category
+#
+ADJ_LIST = []
+NOUN_LIST = []
+ADVERB_LIST = []
+EXCLAMATION_LIST = []
+
+                
+               
+#
+# This function is an implementation of the <REPLACE WITH MADLIB NAME> Madlib.
+#   It will dynamically generate a new Madlib each time it is called by
+#   randomly selecting values from the above lists
+# Return
+# This function will return a String containing the new Madlib
+def generateMadlib():
+
+
+
+
+#
+# Generate the Madlib
+#
+madlib = generateMadlib()
+
+#
+# Print the Madlib
+#
+print(madlib)
+
+
