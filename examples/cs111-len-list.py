54deb31e39e34a943fae6e85745af978dc0acd9e
Luke Hindman <luke@platypuscreations.net>
2017-11-06 19:47:39 -0700

    Added new len() examples to repo

---
+++ examples/cs111-len-list.py
@@ -0,0 +1,16 @@
+#
+# Author:  Luke Hindman
+# Date: Mon Nov  6 19:40:15 MST 2017
+# Description:  Demonstration of using the len() function with strings
+
+# Create a list of items
+# groceryList = []
+groceryList = ["eggs", "milk", "bread", "squash", "cheese"]
+
+# Use the len() function to count how many items
+#   are in the list.  Store the integer result to
+#   the numItems variable
+numItems = len(groceryList)
+
+# Display the number of items in the console
+print(numItems)
---
+++ examples/cs111-len-string.py
@@ -0,0 +1,15 @@
+#
+# Author:  Luke Hindman
+# Date: Mon Nov  6 19:40:15 MST 2017
+# Description:  Demonstration of using the len() function with strings
+
+# Create a string variable
+message = "Hello there"
+
+# Use the len() function to count how many characters
+#   are in the message.  Store the integer result to
+#   the messageLength variable
+messageLength = len(message)
+
+# Display the length of the message in the console
+print(messageLength)
