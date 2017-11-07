f5be41ee7476deadf80c993e69bac4a561c68f7e
Luke Hindman <luke@platypuscreations.net>
2017-11-06 21:09:18 -0700

    Added stub files for MiniTasks 23 and 24

---
+++ minitasks/cs111-splitting-up-stub.py
@@ -0,0 +1,53 @@
+# 
+# Author:
+# Date:
+# Description:
+#
+
+#
+# Single string containing CSV formatted song data
+#  "artist,album,title,duration"
+#  Note: Duration is specified in seconds
+#
+singleSongCSV = "Jimmy Buffett,Songs You Know by Heart,Cheeseburger in Paradise,172"
+
+#
+# List of strings containing CSV formatted song data
+#
+songList = ['Jimmy Buffett,Songs You Know by Heart,Cheeseburger in Paradise,172', 
+            'Jimmy Buffett,Songs You Know by Heart,He Went to Paris,209',
+            'Jimmy Buffett,Songs You Know by Heart,Fins,205',
+            'Jimmy Buffett,Songs You Know by Heart,Son of a Son of a Sailor,205',
+            'Jimmy Buffett,Songs You Know by Heart,A Pirate Looks at Forty,232',
+            'Jimmy Buffett,Songs You Know by Heart,Margaritaville,251',
+            'Jimmy Buffett,Songs You Know by Heart,Come Monday,189',
+            'Jimmy Buffett,Songs You Know by Heart,Changes in Latitudes Changes in Attitudes,195',
+            "Jimmy Buffett,Songs You Know by Heart,Why Don't We Get Drunk,162",
+            'Jimmy Buffett,Songs You Know by Heart,Pencil Thin Mustache,170',
+            'Jimmy Buffett,Songs You Know by Heart,Grapefruit-Juicy Fruit,176',
+            'Jimmy Buffett,Songs You Know by Heart,Boat Drinks,157',
+            'Jimmy Buffett,Songs You Know by Heart,Volcano,218']
+
+# Display nicely formatted song details for a
+#   string provided in the following format:
+#   "Artist,Album,Title,Duration"
+#
+# Parameters
+#   song - String containing comma separated song details
+#
+# Return
+#   none
+def printSong(song):
+ 
+
+
+
+#
+# call printSong() to print singleSongCSV
+#
+
+
+
+#
+# use a for loop and printSong() to print each song in songList
+#
