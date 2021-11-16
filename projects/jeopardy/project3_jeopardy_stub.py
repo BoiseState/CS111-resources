7d23f3af548aa547626382a53645f29443076ee3
krodgers <kathrynrodgers@boisestate.edu>
2021-11-16 14:02:50 -0700

    project 3 jeopardy files

--- projects/jeopardy/project3_jeopardy_stub.py
+++ projects/jeopardy/project3_jeopardy_stub.py
@@ -23,14 +23,16 @@ def getInfo(quesInfo, whichInfo):
 
 
 # Read in a file and return a list containing the lines in the file
-# Strip each line to remove the \n
-# Copy getListFromFile to get you started
+# parameter: filename - name of CSV file to read
+# return: list containing stripped lines of the file
 def loadCSV(filename):
     return []
 
 
 # Takes a list containing every line from the question csv list
 # Returns a new list containing only the categories
+# parameter:  fileList - list containing lines of a CSV file containing quiz data
+# return : list of strings containing unique categories of questions
 def getCategoryList(fileList):
     return []
 
@@ -39,6 +41,10 @@ def getCategoryList(fileList):
 
 # Takes a list containing every line from the question csv list
 # Returns a new list containing only the questions whose category is in categoryList
+# parameter:  fileList - list containing lines of a CSV file containing quiz data
+#             categoryList - list containing categories from which to choose questions
+# return : list of strings containing CSV formatted question info
+#                  from only the categories contained in categoryList
 def getQuestionList(fileList, categoryList):
     return []
 
@@ -50,8 +56,7 @@ def getQuestionList(fileList, categoryList):
 # Q3($200)  Q4($150)  Q5($600)
 # Q6($200)  Q7($150)  Q8($600)
 #
-# Be careful to check if there's actually a question before trying to get the question's value
-# Does not return anything
+# parameter:  questionList - list of 9 CSV formatted questions
 def printBoard(questionList):
     print("PRINTING BOARD")
     
@@ -60,15 +65,16 @@ def printBoard(questionList):
 # Checks question list for a valid question
 # If every element is NO_QUESTION, return false
 # If at least one element has text, return true
+# parameter:  questionList - list of 9 CSV formatted questions
+# return: true if at least one question is unused, false otherwise
 def hasQuestions(questionList):
     return True
 
 
 # Asks user which question he/she wants to answer
-# Validates the number - is it in the right range? Is that element a question (i.e. is not NO_QUESTION)
-# Use a try/except to handle the case the user gives you something that isn't a number
 # See Day 26 slides for example try/except
-# Returns a valid number
+# parameter:  questionList - list of 9 CSV formatted questions
+# return: a valid index into questionList
 def getQuestionIndex(questionList):
     return 0
 
